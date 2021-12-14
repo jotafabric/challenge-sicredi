6ca415e8a1dbb69f2afc6804ae26fcd26b6a56fb
Josias Fabrício <josias@MacBook-Air-A2337.local>
2021-12-13 23:24:11 -0300

    Added the event list view

---
+++ ChallengeSicredi/ChallengeSicredi/List/EventListTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  EventListTableViewCell.swift
+//  ChallengeSicredi
+//
+//  Created by Josias Fabrício on 13/12/21.
+//
+
+import Foundation
+import UIKit
+import Kingfisher
+
+class EventListTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var img: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    
+    var event: Event? {
+        didSet{
+            
+            titleLabel.text = event?.title
+            dateLabel.text = event?.date?.intToDateTime?.dateAndTimetoString()
+            priceLabel.text = event?.price?.currencyValue
+            
+            img.kf.setImage(with: URL(string: event!.image!), placeholder: UIImage(named: "eventImageDefault"), options: nil, progressBlock: nil, completionHandler: nil)
+        }
+    }
+    
+}
+
+
--- ChallengeSicredi/ChallengeSicredi/ViewController.swift
+++ ChallengeSicredi/ChallengeSicredi/ViewController.swift
@@ -7,13 +7,76 @@
 
 import UIKit
 
+protocol EventListDisplayLogic: AnyObject {
+    func displayEvents(viewModel: [Event])
+    func errorLoadingEvents(_ error: String)
+}
+
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var interactor: EventListBusinessLogic?
+    
+    var viewModel: [Event]? {
+        didSet{
+            setupViewFromViewModel()
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        let viewController = self
+        let interactor = EventListInteractor()
+        let presenter = EventListPresenter()
+        //let router = EventListRouter()
+        
+        viewController.interactor = interactor
+        //viewController.router = router
+        interactor.presenter = presenter
+        presenter.viewController = viewController
+        //router.viewController = viewController
+        //router.dataStore = interactor
+        
+        interactor.fetchList()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func setupViewFromViewModel() {
+        tableView.reloadData()
     }
 
 
+}
+
+extension ViewController: EventListDisplayLogic {
+    func displayEvents(viewModel: [Event]) {
+        self.viewModel = viewModel
+        //dismissLoading()
+    }
+    
+    func errorLoadingEvents(_ error: String) {
+        //view.makeToast(error)
+        //dismissLoading()
+    }
+}
+
+extension ViewController: UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return viewModel?.count ?? 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "EventListTableViewCell", for: indexPath) as? EventListTableViewCell
+        
+        if let model = viewModel?[indexPath.row]{
+            cell?.event = model
+        }
+        
+        return cell ?? UITableViewCell()
+    }
 }
 
